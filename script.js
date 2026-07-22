c517c088290d720047d73daa02dfb17869e30c0b
iqos <emelianovstasik2010@gmail.com>
2026-07-22 19:42:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+const USER = "admin";
+
+const PASS = "12345";
+
+function login(){
+
+let user=document.getElementById("username").value;
+
+let pass=document.getElementById("password").value;
+
+if(user===USER && pass===PASS){
+
+document.getElementById("login").style.display="none";
+
+document.getElementById("card").style.display="block";
+
+document.getElementById("music").play();
+
+}else{
+
+document.getElementById("error").innerHTML="Неверный логин или пароль";
+
+}
+
+}
+
+function logout(){
+
+document.getElementById("card").style.display="none";
+
+document.getElementById("login").style.display="block";
+
+let music=document.getElementById("music");
+
+music.pause();
+
+music.currentTime=0;
+
+}
+
+setInterval(()=>{
+
+const heart=document.createElement("div");
+
+heart.innerHTML="❤️";
+
+heart.style.position="fixed";
+
+heart.style.left=Math.random()*100+"vw";
+
+heart.style.top="100vh";
+
+heart.style.fontSize=(20+Math.random()*35)+"px";
+
+heart.style.transition="6s linear";
+
+heart.style.pointerEvents="none";
+
+document.body.appendChild(heart);
+
+setTimeout(()=>{
+
+heart.style.transform="translateY(-120vh)";
+
+heart.style.opacity="0";
+
+},50);
+
+setTimeout(()=>{
+
+heart.remove();
+
+},6000);
+
+},300);
